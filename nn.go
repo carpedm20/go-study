636adb74c4b99378c285d76fe7afef1e5a2888b2
Taehoon Kim <carpedm20@gmail.com>
2014-10-03 11:52:59 +0900

    update nn

--- nn.go
+++ nn.go
@@ -3,19 +3,28 @@ package main
 import (
     "fmt"
     "math"
+    "math/rand"
 )
 
 type Matrix [][]float64
 type Vector []float64
 
 func Rand(a float64, b float64) float64 {
-    return ((b-a)*math.rand.Float64() + a)
+    return ((b-a)*rand.Float64() + a)
 }
 
-func MakeMatrix(I int, J int, fill float64) Matrix {
+func MakeMatrix(I int, J int, fill_opt ...float64) Matrix {
+    fill := 0.0
+    if len(fill_opt) > 0 {
+        fill = fill_opt[0]
+    }
     matrix := make(Matrix, J)
     for i := range matrix {
         matrix[i] = make(Vector, I)
+
+        for j, _ := range matrix[i] {
+            matrix[i][j] = fill
+        }
     }
 
     return matrix
@@ -31,7 +40,7 @@ func Dtransfer(x float64) float64 {
 
 type NN struct {
     // ni: input, nh: hidden nodes, no: output nodes 
-    ni, nh, no float64
+    ni, nh, no int
     // activations for nodes
     ai, ah, ao Vector
     // weights for nodes
@@ -39,8 +48,8 @@ type NN struct {
     regression bool
 }
 
-func New(ni, nh, no float64, regression bool) *NN {
-    nn = new(NN)
+func New(ni, nh, no int, regression bool) *NN {
+    nn := &NN{}
     nn.ni = ni + 1 // +1 for bias node
     nn.nh = nh + 1 // +1 for bias node
     nn.no = no
@@ -53,10 +62,14 @@ func New(ni, nh, no float64, regression bool) *NN {
     nn.wi = MakeMatrix(nn.ni, nn.nh)
     nn.wo = MakeMatrix(nn.nh, nn.no)
 
-    for i:= range nn.nh {
+    for i, col := range nn.wi {
+        for j, _ := range col {
+            nn.wi[i][j] = Rand(-1.0, 1.0)
+            fmt.Println(nn.wi[i][j])
+        }
     }
 
-    return &nn
+    return nn
 }
 
 func main() {
@@ -65,4 +78,7 @@ func main() {
     fmt.Println("123");
     mat := MakeMatrix(3, 4, 0.0)
     fmt.Println(mat[0][0])
+
+    nn := New(2, 3, 1, false)
+    fmt.Println(nn)
 }
